b46d82c6ae47ea036af0ffcf241c93915def5bb5
surya-sukumaran <126955551+surya-sukumaran@users.noreply.github.com>
2023-03-25 13:52:50 +0530

    Update iris_st.py

--- iris_st.py
+++ iris_st.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 from prediction import predict
-
+st.title('Iris classification')
 sl=st.slider('SepalLengthCm',1.0,10.0,0.5)
 sw=st.slider('SepalWidthCm',1.0,10.0,0.5)
 pl=st.slider('PetalLengthCm',1.0,10.0,0.5)
